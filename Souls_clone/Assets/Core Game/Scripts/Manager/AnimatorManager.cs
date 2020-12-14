15df849f979b86c604058ae011642ebd3a17d170
ndinhquan1998 <48425177+ndinhquan1998@users.noreply.github.com>
2020-12-14 21:27:33 +0700

    update

---
+++ Souls_clone/Assets/Core Game/Scripts/Manager/AnimatorManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace DQ
+{
+    public class AnimatorManager : MonoBehaviour
+    {
+        public Animator anim;
+        public void PlayTargetAnimation(string targetAnim, bool isInteracting)
+        {
+            anim.applyRootMotion = isInteracting;
+            anim.SetBool("isInteracting", isInteracting);
+            anim.CrossFade(targetAnim, 0.2f);
+        }
+    }
+}
